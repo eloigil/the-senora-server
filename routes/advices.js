b6dcab4095c712c1dbcc41c89ab75bd53dcb9024
Eloi Gil <eloigil6@gmail.com>
2017-12-08 18:33:03 +0100

    new commit

--- routes/advices.js
+++ routes/advices.js
@@ -6,83 +6,81 @@ const Advice = require('../models/advice').Advice;
 
 /* GET advises for each child. */
 router.get('/advices/:childId', function (req, res, next) {
-    Advice.find({ childID: req.params.childId }, (err, result) => {
-        if (err) {
-            next(err);
-            return;
-        }
-        res.json(result);
-    });
+  Advice.find({ childID: req.params.childId }, (err, result) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(result);
+  });
 });
 
 /* GET favorite advices. */
 router.get('/advices/:childId/favorites', function (req, res, next) {
-    Advice.find({ childID: req.params.childId, favorite: true }, (err, result) => {
-        if (err) {
-            next(err);
-            return;
-        }
-        res.json(result);
-    });
+  Advice.find({ childID: req.params.childId, favorite: true }, (err, result) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(result);
+  });
 });
 
 /* PUT advice. */
 router.put('/advices/:id', function (req, res, next) {
-    Advice.findById(req.params.id, (err, result) => {
-        if (err) {
-            next(err);
-            return;
-        }
-        result.favorite = !result.favorite;
-        result.save((err, data) => {
-            if (err) {
-                next(err);
-                return;
-            }
-            res.json(data)
-        });
-
+  Advice.findById(req.params.id, (err, result) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    result.favorite = !result.favorite;
+    result.save((err, data) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.json(data);
     });
+  });
 });
 
-/*POST new advice*/
+/*POST new advice */
 router.post('/advices', (req, res, next) => {
-    const {
-        title,
-        voice,
-        text,
-        parentID,
-        childID,
-        favorite
-      } = req.body;
+  const {
+    title,
+    voice,
+    text,
+    parentID,
+    childID,
+    favorite
+  } = req.body;
 
+  if (!title) {
+    return response.unprocessable(req, res, 'Missing mandatory field "title".');
+  }
+  if (!voice) {
+    return response.unprocessable(req, res, 'Missing mandatory field "voice".');
+  }
+  if (!text) {
+    return response.unprocessable(req, res, 'Missing mandatory field "text".');
+  }
+  if (!childID) {
+    return response.unprocessable(req, res, 'Select a child');
+  }
+  const newAdvice = Advice({
+    title,
+    voice,
+    text,
+    parentID,
+    childID,
+    favorite
+  });
 
-    if (!title) {
-        return response.unprocessable(req, res, 'Missing mandatory field "title".');
+  newAdvice.save((err) => {
+    if (err) {
+      return next(err);
     }
-    if (!voice) {
-        return response.unprocessable(req, res, 'Missing mandatory field "voice".');
-    }
-    if (!text) {
-        return response.unprocessable(req, res, 'Missing mandatory field "text".');
-    }
-    if (!childID) {
-        return response.unprocessable(req, res, 'Select a child');
-    }
-    const newAdvice = Advice({
-        title,
-        voice,
-        text,
-        parentID,
-        childID,
-        favorite
-    });
-
-    newAdvice.save((err) => {
-        if (err) {
-            return next(err);
-        }
-    });
+  });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,100 +3,99 @@ var router = express.Router();
 
 // User model
 const User = require('../models/user').User;
-//Advice model
+// Advice model
 const Advice = require('../models/advice').Advice;
-
 
 /* GET children. */
 router.get('/user/children', function (req, res, next) {
-    User.find({}).populate('children').exec((err, result) => {
-        if (err) {
-            next(err);
-            return;
-        }
-        res.json(result);
-    });
+  User.find({}).populate('children').exec((err, result) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(result);
+  });
 });
 
 /* POST child. */
 router.post('/user/children', function (req, res, next) {
-    const {
-        name,
-        username,
-        password
-      } = req.body;
+  const {
+    name,
+    username,
+    password
+  } = req.body;
 
-    if (!name) {
-        return res.unprocessable(req, res, 'Missing mandatory field "name".');
+  if (!name) {
+    return res.unprocessable(req, res, 'Missing mandatory field "name".');
+  }
+  if (!username) {
+    return res.unprocessable(req, res, 'Missing mandatory field "username".');
+  }
+  if (!password) {
+    return res.unprocessable(req, res, 'Missing mandatory field "password".');
+  }
+
+  User.findOne({
+    username
+  }, 'username', (err, userExists) => {
+    if (err) {
+      return next(err);
     }
-    if (!username) {
-        return res.unprocessable(req, res, 'Missing mandatory field "username".');
-    }
-    if (!password) {
-        return res.unprocessable(req, res, 'Missing mandatory field "password".');
+    if (userExists) {
+      return response.unprocessable(req, res, 'Username already in use.');
     }
 
-    User.findOne({
-        username
-    }, 'username', (err, userExists) => {
-        if (err) {
-            return next(err);
-        }
-        if (userExists) {
-            return response.unprocessable(req, res, 'Username already in use.');
-        }
+    const salt = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(password, salt);
 
-        const salt = bcrypt.genSaltSync(10);
-        const hashPass = bcrypt.hashSync(password, salt);
-
-        const newUser = User({
-            name,
-            username,
-            password: hashPass,
-            role: 'child'
-        });
-
-        User.create(newUser, (err, result) => {
-            if (err) {
-                return next(err);
-            }
-            const user = req.user;
-            user.children.push(result._id);
-            user.save((err) => {
-                if (err) {
-                    return next(err);
-                }
-                res.json(result);
-            })
-        });
+    const newUser = User({
+      name,
+      username,
+      password: hashPass,
+      role: 'child'
     });
+
+    User.create(newUser, (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      const user = req.user;
+      user.children.push(result._id);
+      user.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.json(result);
+      });
+    });
+  });
 });
 
-/*DELETE child.*/
+/*DELETE child. */
 router.delete('/user/:childId', (req, res, next) => {
-    User.findByIdAndRemove(req.params.childId, (err, result) => {
-        if (err) {
-            return next(err);
-        }
-        User.findById(req.user._id, (err, result) => {
-            if (err) {
-                return next(err);
-            }
-            result.children.splice(result.children.indexOf(req.params.childId), 1);
+  User.findByIdAndRemove(req.params.childId, (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    User.findById(req.user._id, (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      result.children.splice(result.children.indexOf(req.params.childId), 1);
 
-            result.save((err) => {
-                if (err) {
-                    return next(err);
-                }
-                //Remove advices of children
-                // Advice.find({ childId: req.params.childId }, (err, result) => {
-                //     if (err) {
-                //         return next(err);
-                //     }                
-                // });
-            });
-        });
+      result.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        // Remove advices of children
+        // Advice.find({ childId: req.params.childId }, (err, result) => {
+        //     if (err) {
+        //         return next(err);
+        //     }
+        // });
+      });
     });
+  });
 });
 
 module.exports = router;
